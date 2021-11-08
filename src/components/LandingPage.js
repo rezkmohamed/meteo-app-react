5a22bae39b8d8488760af1411232060ce6b2afb2
rezkmohamed <momito310399@gmail.com>
2021-11-08 16:37:12 +0100

    init

---
+++ src/components/LandingPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import classes from "./LandingPage.module.css";
+
+const LandingPage = (props) => {
+    return (
+        <React.Fragment>
+            <div className={classes.main}>
+
+            </div>
+        </React.Fragment>
+    );
+}
+
+export default LandingPage;
